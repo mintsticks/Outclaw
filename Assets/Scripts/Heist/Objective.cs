d249b8d97c51c3b5111726555ec30d76afdcb499
Ali Kedwaii <ali.kedwaii@utexas.edu>
2019-09-08 23:47:51 -0500

    Interactivity with objective

---
+++ Assets/Scripts/Heist/Objective.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Outclaw.Heist
+{
+    public class Objective : MonoBehaviour
+    {
+        private CircleCollider2D collider;
+        private Rigidbody2D rbody;
+        private SpriteRenderer spriteRend;
+        private GameObject promptObj;
+        public GameObject prompt;
+        public float offset;
+
+        private bool isComplete;
+
+        public bool IsComplete
+        {
+            get
+            {
+                return isComplete;
+            }
+            set
+            {
+                isComplete = value;
+                if (isComplete)
+                {
+                    promptObj.GetComponent<SpriteRenderer>().color = Color.green;
+                }
+                else
+                {
+                    promptObj.GetComponent<SpriteRenderer>().color = Color.red;
+                }
+            }
+        }
+
+        public enum InteractableType
+        {
+            NONE,
+            OBJECTIVE,
+            VENT,
+            GUARD,
+            COVER,
+            EXIT
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            collider = GetComponent<CircleCollider2D>();
+            rbody = GetComponent<Rigidbody2D>();
+            spriteRend = GetComponent<SpriteRenderer>();
+            isComplete = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            promptObj = Instantiate(prompt, new Vector2(transform.position.x, transform.position.y + offset),
+            Quaternion.identity, this.transform);
+            if (isComplete)
+            {
+                promptObj.GetComponent<SpriteRenderer>().color = Color.green;
+            }
+            else
+            {
+                promptObj.GetComponent<SpriteRenderer>().color = Color.red;
+                other.gameObject.GetComponent<PlayerController>().IsInteracting = true;
+                other.gameObject.GetComponent<PlayerController>().
+                    InteractWithObject(PlayerController.InteractableType.OBJECTIVE, this.gameObject);
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D other)
+        {
+            Destroy(promptObj);
+            other.gameObject.GetComponent<PlayerController>().IsInteracting = false;
+        }
+
+    }
+}
