9aee3ab4cf76d769598f684eefbc8cf613165fbe
kevin <kevin.lam72003@gmail.com>
2019-09-26 14:58:16 -0500

    added pause to heist context, copied city camera to heist, added visualization of camera bounds

--- Assets/Scripts/UI/StartListener.cs
+++ Assets/Scripts/UI/StartListener.cs
@@ -2,10 +2,13 @@
 using UnityEngine.SceneManagement;
 
 public class StartListener : MonoBehaviour {
+
+  [SerializeField] private string nextScene = "Tutorial";
+
   void Update() {
     //TODO(daniel): replace all inputs to a global player input manager
     if (Input.GetKey(KeyCode.Space)) {
-      SceneManager.LoadScene("Main");
+      SceneManager.LoadScene(nextScene);
     }
   }
 }
