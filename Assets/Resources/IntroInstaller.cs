f70b9a6b40d8529b68fae03bd2754817c213f3d6
Ali Kedwaii <ali.kedwaii@utexas.edu>
2019-09-30 18:49:40 -0500

    Intro Scene textonly version

---
+++ Assets/Resources/IntroInstaller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Outclaw.City;
+using UnityEngine;
+using Zenject;
+
+namespace Outclaw {
+  public class IntroInstaller : MonoInstaller {
+
+    [SerializeField] private GameObject playerPrefab;
+    
+    /// <summary>
+    /// For all classes common to city scenes.
+    /// Bind the interfaces to the concrete classes.
+    /// </summary>
+    public override void InstallBindings() {
+      BindComponents();
+    }
+
+    private void BindComponents()
+    {
+      Container.Bind<IPlayer>()
+        .To<Player>()
+        .FromComponentInNewPrefab(playerPrefab)
+        .AsSingle()
+        .NonLazy();
+    }
+  }
+}
---
+++ Assets/Scripts/IntroDialogueUI.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Outclaw {
+  public class IntroDialogueUI : MonoBehaviour {
+
+    [SerializeField] private float textSpeed = 0.025f;
+
+    [SerializeField] private Text introText;
+
+    [SerializeField] private string[] lines;
+
+    [Inject] private IPlayerInput playerInput;
+
+    public void Start()
+    {
+      StartCoroutine(PrintLines(lines));
+    }
+
+    public void Update()
+    {
+      
+    }
+
+    private IEnumerator PrintLines(string[] lines) {
+      foreach (string line in lines)
+      {
+        yield return StartCoroutine(PrintLine(line));
+      }
+
+      SceneManager.LoadScene("Main");
+    }
+    private IEnumerator PrintLine(string line) {
+      StringBuilder stringBuilder = new StringBuilder();
+      introText.text = stringBuilder.ToString();
+      yield return new WaitForSeconds(textSpeed);
+      float time = 0.0f;
+      foreach (char c in line)
+      {
+        while (time < textSpeed)
+        {
+          time += Time.deltaTime;
+
+          if (playerInput.IsInteractDown())
+          {
+            introText.text = line;
+            break;
+          }
+          
+          yield return null;
+        } 
+        
+        time = 0.0f;
+
+        if (!playerInput.IsInteractDown())
+        {
+          stringBuilder.Append(c);
+          introText.text = stringBuilder.ToString();
+          yield return null;
+        }
+        else
+        {
+          introText.text = line;
+          yield return null;
+          break;
+        }
+        
+      }
+    
+      yield return NextLine();
+    }
+
+    private IEnumerator NextLine() {
+      while (!playerInput.IsInteractDown()) {
+        yield return null;
+      }
+    }
+    
+  }
+
+}
