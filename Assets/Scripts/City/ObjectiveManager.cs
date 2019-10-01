ecb78be57cd9cebf648c6777a29507440b2980dc
danielw <daniel.wong@utexas.edu>
2019-10-01 14:33:28 -0500

    update the home scene

---
+++ Assets/Scripts/City/ObjectiveManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Outclaw;
+using Outclaw.City;
+using UnityEngine;
+using Zenject;
+
+namespace City {
+  public class ObjectiveManager : MonoBehaviour {
+    [SerializeField]
+    private List<GameStateObjectives> objectivesForGameStates;
+
+    [Inject]
+    private IGameStateManager gameStateManager;
+    
+    private Dictionary<GameState, List<ObjectType>> completedObjectives;
+
+    private void Awake() {
+      completedObjectives = new Dictionary<GameState, List<ObjectType>>();
+    }
+     
+    public void CompleteObjective(ObjectType type) {
+      
+    }
+  }
+
+  [Serializable]
+  public class GameStateObjectives {
+    public GameState _state;
+    public List<ObjectType> objectives;
+  }
+}
