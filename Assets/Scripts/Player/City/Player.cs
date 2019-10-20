2732f1694df58116996a98eb8be48e18c4dce25d
kevin <kevin.lam72003@gmail.com>
2019-10-20 17:15:58 -0500

    started file reorganization

---
+++ Assets/Scripts/Player/City/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Outclaw.City {
+  public interface IPlayer {
+    Transform PlayerTransform { get; }
+    Vector3 PlayerVelocity { get; }
+  }
+  
+  public class Player : MonoBehaviour, IPlayer {
+    [SerializeField]
+    private MovementController movementController;
+
+    [SerializeField]
+    private InteractionController interactionController;
+    
+    [Inject]
+    private IPlayerData playerData;
+
+    [Inject]
+    private IDialogueManager dialogueManager;
+    
+    public Transform PlayerTransform => transform;
+
+    public Vector3 PlayerVelocity => movementController.Velocity;
+
+    void FixedUpdate() {
+      movementController.UpdatePhysics();
+    }
+
+    void Update() {
+      movementController.UpdateMovement();
+      if (dialogueManager.IsDialogueRunning) {
+        return;
+      }
+      interactionController.UpdateInteraction();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+      interactionController.HandleEnter(other);
+    }
+    
+    private void OnTriggerExit2D(Collider2D other) {
+      interactionController.HandleExit(other);
+    }
+  }
+}
---
+++ Assets/Scripts/Player/MovementController.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Outclaw.City {
+  /// <summary>
+  /// Updates the players position and appearance in the city.
+  /// </summary>
+  public class MovementController : MonoBehaviour {
+    [SerializeField]
+    private CharacterController2D controller;
+    
+    [SerializeField]
+    private PlayerAnimController ac;
+    
+    [SerializeField] 
+    private Rigidbody2D rb;
+
+    [SerializeField] 
+    private float jumpHeight;
+
+    [SerializeField]
+    private float gravity;
+
+    [SerializeField]
+    private float runSpeed;
+
+    [SerializeField]
+    private float groundDamping;
+    
+    [SerializeField]
+    private float inAirDamping;
+    
+    [Inject]
+    private IPlayerInput playerInput;
+
+    [Inject]
+    private IDialogueManager dialogueManager;
+    
+    private Vector3 velocity;  
+    public Vector3 Velocity => velocity;
+    
+    private bool isJumping;
+    private bool isDescending;
+
+    public void UpdateMovement() {
+      UpdateHorizontal();
+      UpdateVertical();
+      UpdateAnimationState(velocity);
+    }
+
+    public void UpdatePhysics() {
+      controller.Move(velocity * Time.fixedDeltaTime, ref isJumping, ref isDescending);
+      velocity = controller.Velocity;
+    }
+    
+    private void UpdateHorizontal() {
+      var moveDir = MoveDirection();
+      var dampingFactor = controller.isGrounded ? groundDamping : inAirDamping;
+      velocity.x = Mathf.Lerp(velocity.x, moveDir * runSpeed, Time.deltaTime * dampingFactor);
+
+      if (moveDir == 0) {
+        return;
+      }
+
+      var scale = rb.transform.localScale;
+      if ((moveDir < 0 && scale.x > 0) || (moveDir > 0 && scale.x < 0)) {
+        rb.transform.localScale = new Vector3(-scale.x, scale.y, scale.z);
+      }
+    }
+
+    private int MoveDirection() {
+      if (dialogueManager.IsDialogueRunning) {
+        return 0;
+      }
+      return playerInput.IsLeft() ? -1 : playerInput.IsRight() ? 1 : 0;
+    }
+
+    private void UpdateVertical() {
+      CheckDescend();
+      CheckJump();
+      velocity.y += gravity * Time.deltaTime;
+    }
+
+    private void CheckJump() {
+      if (!controller.isGrounded || dialogueManager.IsDialogueRunning) {
+        return;
+      }
+
+      if (playerInput.IsJumpDown()) {
+        isJumping = true;
+      }
+      
+      velocity.y = isJumping ? Mathf.Sqrt(2f * jumpHeight * -gravity) : velocity.y;
+    }
+
+    private void CheckDescend() {
+      if (!controller.isGrounded || dialogueManager.IsDialogueRunning) {
+        return;
+      }
+      if (playerInput.IsDownPress()) {
+        isDescending = true;
+      }
+    }
+
+    private void UpdateAnimationState(Vector3 move) {
+      ac.SetHorizontalVelocity(Mathf.Abs(move.x));
+      ac.SetVerticalVelocity(controller.isGrounded ? 0 : move.y);
+      ac.SetIsLanding(move.y <= 0 && controller.IsNearGround(Mathf.Abs(gravity)));
+    }
+  }
+}
