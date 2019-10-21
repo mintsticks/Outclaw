e761c6ad0ff30734b35aaca06a0d00ded144a294
Ali Kedwaii <ali.kedwaii@utexas.edu>
2019-10-20 20:54:39 -0500

    modifying xbox input

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -26,88 +26,88 @@ namespace Outclaw {
 
   public class PlayerInput : IPlayerInput{
     public bool IsLeft() {
-      return Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
-//      return Input.GetAxis("Horizontal") < 0;
+//      return Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+      return Input.GetAxis("Horizontal") < 0;
     }
 
     public bool IsLeftDown() {
-      return Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A);
-//      return Input.GetAxis("Horizontal") < 0;
+//      return Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A);
+      return Input.GetAxis("Horizontal") < 0;
     }
 
     public bool IsRight() {
-      return Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
-//      return Input.GetAxis("Horizontal") > 0;
+//      return Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+      return Input.GetAxis("Horizontal") > 0;
     }
     
     public bool IsRightDown() {
-      return Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D);
-//      return Input.GetAxis("Horizontal") > 0;
+//      return Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D);
+      return Input.GetAxis("Horizontal") > 0;
     }
 
     public bool IsUp() {
-      return Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
-//      return Input.GetAxis("Vertical") > 0;
+//      return Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+      return Input.GetAxis("Vertical") > 0;
     }
 
     public bool IsUpPress() {
-      return Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W);
-//      return Input.GetAxis("Vertical") > 0;
+//      return Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W);
+      return Input.GetAxis("Vertical") > 0;
     }
     
     public bool IsDown() {
-      return Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
-//      return Input.GetAxis("Vertical") < 0;
+//      return Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
+      return Input.GetAxis("Vertical") < 0;
     }
 
     public bool IsDownPress() {
-      return Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S);
-//      return Input.GetAxis("Vertical") < 0;
+//      return Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S);
+      return Input.GetAxis("Vertical") < 0;
     }
     
     public bool IsJump() {
-      return Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
-//      return Input.GetAxis("Vertical") > 0;
+//      return Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+      return Input.GetButton("Jump");
     }
 
     public bool IsJumpDown() {
-      return Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W);
-//      return Input.GetAxis("Vertical") > 0;
+//      return Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W);
+      return Input.GetButtonDown("Jump");
     }
 
     public bool IsInteract() {
-      return Input.GetKey(KeyCode.E);
-//      return Input.GetButton("Fire1");
+//      return Input.GetKey(KeyCode.E);
+      return Input.GetButton("Interact");
     }
     
     public bool IsInteractDown() {
-      return Input.GetKeyDown(KeyCode.E);
-//      return Input.GetButtonDown("Fire1");
+//      return Input.GetKeyDown(KeyCode.E);
+      return Input.GetButtonDown("Interact");
     }
 
     public bool IsAbility() {
-      return Input.GetKey(KeyCode.R);
-//      return Input.GetButton("Fire3");
+//      return Input.GetKey(KeyCode.R);
+      return Input.GetButton("Ability");
     }
 
     public bool IsSense() {
-      return Input.GetKey(KeyCode.F);
-//      return Input.GetButton("Fire2");
+//      return Input.GetKey(KeyCode.F);
+      return Input.GetButton("Sense");
     }
     
     public bool IsSenseDown() {
-      return Input.GetKeyDown(KeyCode.F);
-//      return Input.GetButtonDown("Fire2");
+//      return Input.GetKeyDown(KeyCode.F);
+      return Input.GetButtonDown("Sense");
     }
     
     public bool IsSenseUp() {
-      return Input.GetKeyUp(KeyCode.F);
-//      return Input.GetButtonUp("Fire2");
+//      return Input.GetKeyUp(KeyCode.F);
+      return Input.GetButtonUp("Sense");
     }
 
     public bool IsPauseDown() {
-      return Input.GetKeyDown(KeyCode.Escape);
-//      return Input.GetButtonDown("Pause");
+//      return Input.GetKeyDown(KeyCode.Escape);
+      return Input.GetButtonDown("Pause");
     }
   }
 }
