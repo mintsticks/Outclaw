16c429df08a7a43f34b1b0bc3e21aa0a239c4418
kevin <kevin.lam72003@gmail.com>
2019-11-16 17:36:12 -0600

    updated home to use door prefabs

---
+++ Assets/Scripts/Managers/Sense/Heist/ShowCanvasOnSense.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Outclaw.Heist{
+  public class ShowCanvasOnSense : MonoBehaviour, ISenseElement {
+    
+    [SerializeField] private CanvasGroup canvas;
+
+    [Inject] private ISenseVisuals senseVisuals;
+
+    private void Awake() {
+      senseVisuals.RegisterSenseElement(this);
+
+      canvas.alpha = 0;
+    }
+
+    public void UpdateElement(float animationProgress) {
+      canvas.alpha = animationProgress;
+    }
+
+    public void OnActivate() { }
+    public void OnDeactivate() { }
+  }
+}
