943751fdb4bd9b471b5e1cc2df96f9b397fb3567
kevin <kevin.lam72003@gmail.com>
2019-11-17 23:21:53 -0600

    menu and landing sounds

--- Assets/Scripts/Enemies/Detection/AttentionZone.cs
+++ Assets/Scripts/Enemies/Detection/AttentionZone.cs
@@ -7,28 +7,41 @@ using Zenject;
 namespace Outclaw.Heist {
   public class AttentionZone : MonoBehaviour {
     [SerializeField] private OnDetect onDetect = new OnDetect();
+    [SerializeField] private UnityEvent onDetectStay = new UnityEvent();
     [SerializeField] private UnityEvent onDetectLoss = new UnityEvent();
 
     [Inject] private ISneakManager sneakManager;
     [Inject] private IHideablePlayer hideablePlayer;
+
+    private bool detectedLastFrame = false;
+
     public void EnterAttention() {
       if (sneakManager.IsSneaking || hideablePlayer.Hidden) {
         return;
       }
       onDetect.Invoke();
+      detectedLastFrame = true;
     }
 
     public void StayAttention(){
       if (sneakManager.IsSneaking || hideablePlayer.Hidden) {
         onDetectLoss.Invoke();
+        detectedLastFrame = false;
+        return;
+      }
+      
+      if(detectedLastFrame){
+        onDetectStay.Invoke();
         return;
       }
 
       onDetect.Invoke();
+      detectedLastFrame = true;
     }
 
     public void ExitAttention() {
       onDetectLoss.Invoke();
+      detectedLastFrame = false;
     }
   }
 }
