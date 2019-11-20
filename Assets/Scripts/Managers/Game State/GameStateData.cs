1757ca158fc8c89f62dad3f9105f9424ea8f6010
Ali Kedwaii <ali.kedwaii@utexas.edu>
2019-11-20 14:13:11 -0600

    view objective in pause

--- Assets/Scripts/Managers/Game State/GameStateData.cs
+++ Assets/Scripts/Managers/Game State/GameStateData.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 
 namespace Outclaw{
   [CreateAssetMenu(fileName = "Game State Data", menuName = "Outclaw/Game State Data")]
-  public class GameStateData : ScriptableObject
-  {
+  public class GameStateData : ScriptableObject {
+    [SerializeField] private string objectiveText;
+    public string ObjectiveText => objectiveText;
+
     public List<GameStateObjectives> childStates;
 
     public bool HasCompleteObjective{
---
+++ Assets/Scripts/UI/Menus/Pause Menu/CurrentObjectiveDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+using Outclaw.UI;
+using Utility;
+
+namespace Outclaw {
+  public class CurrentObjectiveDisplay : MonoBehaviour {
+
+    [Inject] private IGameStateManager gameStateManager;
+
+    [SerializeField] private Text objectiveText;
+
+    private GameStateData gameStateData;
+
+    void Awake() {
+      gameStateData = gameStateManager.CurrentGameStateData;
+      UpdateDisplay();
+    }
+
+    void Update() {
+      if (gameStateData == gameStateManager.CurrentGameStateData) {
+        return;
+      }
+      gameStateData = gameStateManager.CurrentGameStateData;
+      UpdateDisplay();
+    }
+
+    private void UpdateDisplay() {
+      objectiveText.text = gameStateData.ObjectiveText;
+    }
+  }
+}
