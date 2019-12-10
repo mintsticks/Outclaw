d24fe00381f4a7c5898cb9b65bdb30b4edc7b73f
kevin <kevin.lam72003@gmail.com>
2019-12-09 18:23:31 -0600

    mostly working location graph

---
+++ Assets/Scripts/InitLocation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Outclaw{
+  public class InitLocation : MonoBehaviour
+  {
+    [SerializeField] LocationData location;
+    [Inject] City.ILocationManager locationManager;
+
+    void Start(){
+      if(locationManager.CurrentLocation == null){
+        locationManager.CurrentLocation = location;
+      }
+    }
+  }
+}
